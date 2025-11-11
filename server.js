a7a556ee3cffc220e7ed58be68b80e5a23db7c92
elisabichiappini <elisabichiappini@gmail.com>
2025-11-11 23:29:26 +0100

    feat: add host port e body, chiamata corretta al server

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-'use strict';
+const http = require('http');
+const dotenv = require('dotenv');
+dotenv.config();
+const port = process.env.PORT || 'NESSUNA PORTA';
+const host = process.env.HOST || 'nessun hosting';
+
+const body = process.env.BODY || '<h1>body non trovato</h1>';
+http.createServer((req, res) => {
+    res.writeHead(200, {
+        'Content-type': 'text/html'
+    });
+    res.end(body);
+}).listen(port, host, () => {
+    console.log(`Server attivato su http://${host}:${port}`)
+});
